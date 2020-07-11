e8337d5230d37b3952396b548debc4155ea8487b
Petr Brovkin <bropet@mail.ru>
2020-07-11 17:31:18 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, './gh-pages/dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   module: {
     rules: [
